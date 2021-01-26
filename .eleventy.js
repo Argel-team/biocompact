e3f548b2cac452949bd773ff1757e23faed78b86
Timur <webmcheck@gmail.com>
2021-01-26 12:20:44 +0300

    init

---
+++ .eleventy.js
@@ -0,0 +1,18 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.setBrowserSyncConfig({
+    host: 'biocompact.local',
+    port: '9090'
+  })
+
+  eleventyConfig.addPassthroughCopy({'src/assets': '/assets'})
+  eleventyConfig.addPassthroughCopy({'src/wp-content': '/wp-content'})
+
+  return {
+    dir: {
+      input: 'src/views',
+      output: 'dist',
+      includes: '../includes',
+      layouts: '../layouts'
+    }
+  }
+}
